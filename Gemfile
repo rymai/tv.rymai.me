fe564b63f571e18fa8aa8d2eb28c6736d95bec62
Rémy Coutable <rymai@rymai.me>
2011-10-22 15:35:14 +0200

    Remove Rails files and regenerate pages

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.3'
+gem 'jekyll'
+
+gem 'RedCloth'
+
+# gem 'rails', '3.0.3'
 
 # gem 'sqlite3-ruby', :require => 'sqlite3'
 
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RymaiDotTv::Application.config.secret_token = 'f7961d6ab779cf691a756684ccbb09ebdce9d4b1dbc7853a3d4a8e0e8e1fdf5c9be0327aade04305fe3559c93db814d4b21c932340148d6564d7f29bdccef4d4'
